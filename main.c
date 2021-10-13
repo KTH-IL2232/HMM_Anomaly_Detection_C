7bc91f2145b5dba0e65ea58e69dbc968fe0574f9
DeyangJiang <54866770+DeyangJiang@users.noreply.github.com>
2021-10-14 00:17:34 +0200

    failed revert

--- main.c
+++ main.c
@@ -4,12 +4,11 @@ int main()
 {
 	int r;
 	int n_components, n_features;
-	r = read_hmm_size("./Model_Parameters", &n_components, &n_features);
-	long double* transition = malloc(n_components * n_components);
-	long double* emission = malloc(n_components * n_features);
-	long double* initial = malloc(n_components);
-	printf("num of components: %i & num of features: %i\n", n_components, n_features);
-	r = read_hmm_coef("./Model_Parameters", n_components, n_features, transition, emission, initial);
-	printf("coef[0]: %g\n", transition[0]);
+	long double* transition;
+	long double* emission;
+	long double* initial;
+	//printf(" %g\n", n_features);
+	r = read_hmm_coef("", &n_components, &n_features, &transition, &emission, &initial);
+	
 	return 0;
 }
