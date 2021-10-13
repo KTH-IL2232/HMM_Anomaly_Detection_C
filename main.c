dfe792b8e37b3ac5d16cf67be06fda516ff375f4
DeyangJiang <54866770+DeyangJiang@users.noreply.github.com>
2021-10-13 23:55:36 +0200

    Revert "Reading parameters from file."
    This reverts commit 36cf26a2708b03f945212e58dd0630bcb404c42a.

--- main.c
+++ main.c
@@ -1,37 +1,15 @@
 #include <stdio.h>
-#include <stdlib.h>
-
 #include "fileio.h"
-
 int main()
 {
-    // Windows path
-    // const char *path_hmm = "./Model_Parameters";
-    // MAC path
-    const char *path_hmm = "/Users/yang/Desktop/IL2232/MyCode/CVersionHMM/HMM_Anomaly_Detection_C/Model_Parameters";
-    
-    // read the number of components and number of features
+	int r;
 	int n_components, n_features;
-    read_hmm_size(path_hmm, &n_components, &n_features);
-    
-    // read pi, A, B
-	long double* transition_mat = malloc(n_components * n_components
-                                         * sizeof(long double));
-	long double* emission_mat = malloc(n_components * n_features
-                                       * sizeof(long double));
-	long double* initial_mat = malloc(n_components
-                                      * sizeof(long double));
-	read_hmm_coef(path_hmm, transition_mat, emission_mat, initial_mat);
-    
-    printf("T[0] = %.10Lf, T[end] = %.10Lf\n",
-           transition_mat[0], transition_mat[899]);
-    printf("E[0] = %.10Lf, E[end] = %.10Lf\n",
-           emission_mat[0], emission_mat[239]);
-    printf("I[0] = %.10Lf, I[end] = %.10Lf\n",
-           initial_mat[0], initial_mat[29]);
-    
-    free(transition_mat);
-    free(emission_mat);
-    free(initial_mat);
+	r = read_hmm_size("./Model_Parameters", &n_components, &n_features);
+	long double* transition = malloc(n_components * n_components);
+	long double* emission = malloc(n_components * n_features);
+	long double* initial = malloc(n_components);
+	printf("num of components: %i & num of features: %i\n", n_components, n_features);
+	r = read_hmm_coef("./Model_Parameters", n_components, n_features, transition, emission, initial);
+	printf("coef[0]: %g\n", transition[0]);
 	return 0;
 }
