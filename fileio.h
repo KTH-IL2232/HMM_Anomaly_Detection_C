b2c3cbdbd9779ea2cc80554a75ef4b6864752e1c
DeyangJiang <54866770+DeyangJiang@users.noreply.github.com>
2021-10-13 23:55:36 +0200

    nothing happens
    nothing happens
    
    Nothing happens
    
    Revert "Reading parameters from file."
    
    This reverts commit 36cf26a2708b03f945212e58dd0630bcb404c42a.

--- fileio.h
+++ fileio.h
@@ -10,10 +10,8 @@
 // EFFECTS: reads the file N_Components.txt and N_Features.txt
 //          stores the parameters in *num_components, *num_features
 //          returns 0 on success, -1 on failure
-int read_hmm_size(const char *path_hmm,
-                  int *num_components, int *num_features);
-
-int read_hmm_coef(const char *path_hm, long double* transition_mat,
-                  long double* emission_mat, long double* initial_mat);
-
+int read_hmm_size(const char* path_hmm,
+    int* num_components, int* num_features);
+int read_hmm_coef(const char* path_hm, long double* transition_mat,
+    long double* emission_mat, long double* initial_mat);
 #endif
--- main.c
+++ main.c
@@ -5,33 +5,33 @@
 
 int main()
 {
-    // Windows path
-    // const char *path_hmm = "./Model_Parameters";
-    // MAC path
-    const char *path_hmm = "/Users/yang/Desktop/IL2232/MyCode/CVersionHMM/HMM_Anomaly_Detection_C/Model_Parameters";
-    
+        // Windows path
+         const char *path_hmm = "./Model_Parameters";
+        // MAC path
+        //const char* path_hmm = "/Users/yang/Desktop/IL2232/MyCode/CVersionHMM/HMM_Anomaly_Detection_C/Model_Parameters";
+
     // read the number of components and number of features
-	int n_components, n_features;
-    read_hmm_size(path_hmm, &n_components, &n_features);
-    
-    // read pi, A, B
-	long double* transition_mat = malloc(n_components * n_components
-                                         * sizeof(long double));
-	long double* emission_mat = malloc(n_components * n_features
-                                       * sizeof(long double));
-	long double* initial_mat = malloc(n_components
-                                      * sizeof(long double));
-	read_hmm_coef(path_hmm, transition_mat, emission_mat, initial_mat);
-    
-    printf("T[0] = %.10Lf, T[end] = %.10Lf\n",
-           transition_mat[0], transition_mat[899]);
-    printf("E[0] = %.10Lf, E[end] = %.10Lf\n",
-           emission_mat[0], emission_mat[239]);
-    printf("I[0] = %.10Lf, I[end] = %.10Lf\n",
-           initial_mat[0], initial_mat[29]);
-    
-    free(transition_mat);
-    free(emission_mat);
-    free(initial_mat);
-	return 0;
+        int n_components, n_features;
+        read_hmm_size(path_hmm, &n_components, &n_features);
+
+        // read pi, A, B
+        long double* transition_mat = malloc(n_components * n_components
+            * sizeof(long double));
+        long double* emission_mat = malloc(n_components * n_features
+            * sizeof(long double));
+        long double* initial_mat = malloc(n_components
+            * sizeof(long double));
+        read_hmm_coef(path_hmm, transition_mat, emission_mat, initial_mat);
+
+        printf("T[0] = %g, T[end] = %g\n",
+            transition_mat[0], transition_mat[899]);
+        printf("E[0] = %g, E[end] = %g\n",
+            emission_mat[0], emission_mat[239]);
+        printf("I[0] = %g, I[end] = %g\n",
+            initial_mat[0], initial_mat[29]);
+
+        free(transition_mat);
+        free(emission_mat);
+        free(initial_mat);
+        return 0;
 }
